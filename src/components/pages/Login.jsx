44eed07dfe18dedb44280351a80b783fb2ffa71a
kent0221 <kent.mosstar@gmail.com>
2024-12-18 11:23:55 +0900

    ルーティングの設定

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,21 @@
+/* 
+** Login.jsx;
+*/ 
+
+import { memo } from 'react';
+// import PropTypes from 'prop-types';
+
+export const Login = memo(() => {
+  // props
+  // Context
+  // hooks
+  // State
+  // function
+  return (
+    <>
+      <h1>Login</h1>
+    </>
+  );
+});
+Login.displayName = 'Login';
+Login.propTypes = {};
